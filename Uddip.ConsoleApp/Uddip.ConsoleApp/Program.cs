546c84dea3e6fd8e97e32e456f235dc5b2d4ea9e
Uddip Mitra <uddipmitra@gmail.com>
2020-03-19 01:18:26 -0400

    Add console app and add nuget packages

---
+++ Uddip.ConsoleApp/Uddip.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WiredBrainCoffee.Simulators;
+
+namespace Uddip.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coffeeMachine = new CoffeeMachine();
+            coffeeMachine.MakeCappuccino();
+            coffeeMachine.MakeCappuccino();
+
+            Console.WriteLine($"Cappuccino number {coffeeMachine.CounterCappuccino}");
+
+            coffeeMachine.ShowStoredState();
+        }
+    }
+}
